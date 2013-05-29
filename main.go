4260c416e7661a30314bb33a5449fb29b8de5580
sdevil507 <sdevil507@163.com>
2013-05-29 16:21:07 +0800

    golang helloworld!

---
+++ main.go
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator(Feng)
+ * Date: 13-5-27
+ * Time: 下午3:29
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import (
+	"fmt"
+	)
+
+func main() {
+	fmt.Println("中文支持不!")
+	s:=123
+	fmt.Printf("%d\n",s)
+}
